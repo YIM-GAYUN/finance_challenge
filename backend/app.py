b3eb66e1355f2108a1662a656726eb9a4c26f2a1
YIM-GAYUN <yunning.yim@gmail.com>
2025-08-14 23:56:29 +0900

    modify address

--- backend/app.py
+++ backend/app.py
@@ -44,7 +44,8 @@ print(f"Loaded Finnhub API Key: {bool(FINNHUB_API_KEY)}")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173", "http://localhost:5173", "https://finance-challenge-psi.vercel.app"],
+    # allow_origins=["http://127.0.0.1:5173", "http://localhost:5173", "https://finance-challenge-psi.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
